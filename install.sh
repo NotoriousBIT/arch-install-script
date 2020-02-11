1a7491c389f30ac025ed73d812f09c1e0de147ee
notoriousbit <>
2020-02-11 12:51:55 +0100

    Adding sources

--- install.sh
+++ install.sh
@@ -1,2 +1,70 @@
 #!/bin/sh
-echo "Hello world"
+echo "# Setting keyboard layout"
+loadkeys de-latin1
+
+echo "# Setting system time"
+timedatectl set-ntp true
+
+while true; do
+  read -p "Do you wish to create any partitions? [Y/n] " yn
+  case $yn in
+          [Yy][eE][sS]|[yY] ) cfdisk; break;;
+          [Nn][Oo]|[nN] ) break;;
+          * ) echo "Please answer yes or no.";;
+  esac
+done
+
+echo ""
+echo "# lsblk"
+echo "+ lsblk -o name,mountpoint,label,size,uuid"
+echo ""
+lsblk -o name,mountpoint,label,size,uuid
+
+echo ""
+echo "# Partitioning"
+echo "#"
+echo "# Examples:"
+echo "# mkfs.ext4 -L arch /dev/sda1"
+echo ""
+
+while true; do
+  read -p "Run command or quit with ':q': " cmd
+  case $cmd in
+          [:][q] ) break;;
+          * ) $cmd;;
+  esac
+done
+
+read -p "Specify the name of the root partition! " partition
+mount /dev/$partition /mnt
+echo "# /dev/$partition mounted at /mnt"
+# todo mounting of /var /home /boot ...
+
+echo ""
+echo "# Setting mirroslist"
+wget -N https://raw.githubusercontent.com/NotoriousBIT/arch-install-script/master/mirrorlist -O /etc/pacman.d/mirrorlist
+
+echo ""
+echo " Installing packages"
+pacstrap /mnt base linux linux-firmware dhcpcd networkmanager wget vim
+
+echo ""
+echo " fstab"
+genfstab -U /mnt >> /mnt/etc/fstab
+
+echo ""
+echo " chroot"
+wget -N 192.168.56.1:9000/install_chroot.sh -O /mnt/install_chroot.sh
+arch-chroot /mnt sh install_chroot.sh
+read input
+
+umount /mnt
+
+while true; do
+  read -p "Do you wish to reboot? [Y/n] " yn
+  case $yn in
+          [Yy][eE][sS]|[yY] ) reboot; break;;
+          [Nn][Oo]|[nN] ) break;;
+          * ) echo "Please answer yes or no.";;
+  esac
+done
---
+++ install_chroot.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+echo ""
+read -p "Specify the hostname: " hostname
+echo $hostname > /etc/hostname
+echo "127.0.0.1	localhost" > /etc/hosts
+echo "::1 localhost" >> /etc/hosts
+echo "127.0.0.1	$hostname.localdomain $hostname" >> /etc/hosts
+
+echo ""
+echo " enable dhcpcd"
+systemctl enable dhcpcd
+
+echo ""
+echo " enable networkmanager"
+systemctl enable NetworkManager
+
+echo ""
+echo " setting locale.conf"
+echo LANG=de_DE.UTF-8 > /etc/locale.conf
+
+echo ""
+echo " setting locale.gen"
+wget -N https://raw.githubusercontent.com/NotoriousBIT/arch-install-script/master/locale.gen -O /etc/locale.gen
+locale-gen
+
+echo ""
+echo " setting vconsole.conf"
+echo KEYMAP=de-latin1 > /etc/vconsole.conf
+echo FONT=lat9w-16 >> /etc/vconsole.conf
+
+echo ""
+echo " setting localtime"
+ln -sf /usr/share/zoneinfo/Europe/Berlin /etc/localtime
+
+echo ""
+echo " setting root password:"
+passwd
+
+echo ""
+echo " installing grub"
+pacman -S --noconfirm grub
+grub-install /dev/sda
+grub-mkconfig -o /boot/grub/grub.cfg
