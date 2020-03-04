e7ff123f84710d07f4f158d21ad552cdea294568
Jan <jan@erichsen-web.de>
2020-03-04 21:02:11 +0100

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -23,8 +23,8 @@ done
 # 2 100% main
 (echo o; echo d; echo n; echo p; echo 1; echo ""; echo "+500M"; echo n; echo p; echo 2; echo ""; echo ""; echo w) | fdisk $install_device 
 
-mkfs.ext2 $install_device1
+mkfs.ext2 $install_device+"1"
 
 # Setup the encryption of the system
-cryptsetup -c aes-xts-plain64 -y --use-random luksFormat $install_device2
-cryptsetup luksOpen $install_device2 luks
+cryptsetup -c aes-xts-plain64 -y --use-random luksFormat $install_device+"2"
+cryptsetup luksOpen $install_device+"2" luks
