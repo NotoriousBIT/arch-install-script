a72f1a5a92b78bb768c407a796c5ed1d4beb0761
Jan <jan@erichsen-web.de>
2020-03-04 20:08:52 +0100

    Create install2.sh

---
+++ install2.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+echo "# Setting keyboard layout"
+loadkeys de-latin1
+
+echo "# Setting system time"
+timedatectl set-ntp true
+
+echo "# lsblk"
+lsblk
+
+while true; do
+  read -p "Specify the name of the device to install to (i.e. /dev/sda) " install_device
+  read -p "You selcted /"$install_device/", is this correct? [Y/n] " yn
+  case $yn in
+          [Yy][eE][sS]|[yY] ) break;;
+          [Nn][Oo]|[nN] ) ;;
+          * ) echo "Please answer yes or no.";;
+  esac
+done
