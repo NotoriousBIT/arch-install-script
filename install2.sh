576f6b4d5bd4baf52449f8ee10fc303287231e94
Jan <jan@erichsen-web.de>
2020-03-10 20:58:10 +0100

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -19,11 +19,33 @@ while true; do
 done
 
 # Create partitions
-# 1 500MB Boot
-# 2 100% main
+# 1 500MB EFI
+# 2 500MB Boot
+# 3 100% main
+
+# Get the corrosponding numbers of fdisk partition types
+EFI_SYSTEM_NUMBER=(echo l; echo q) | sudo fdisk /dev/sdc | grep -oP "[0-9]+(?= EFI System)"
+LINUX_FILESYSTEM_NUMBER=(echo l; echo q) | sudo fdisk /dev/sdc | grep -oP "[0-9]+(?= Linux filesystem )"
+LINUX_LVM_NUMBER=(echo l; echo q) | sudo fdisk /dev/sdc | grep -oP "[0-9]+(?= Linux LVM)"
+
+# Wipe the disk
 wipefs -af $install_device
-(echo o; echo w) | fdisk $install_device
-(echo d; echo n; echo p; echo 1; echo ""; echo "+500M"; echo a; echo n; echo p; echo 2; echo ""; echo ""; echo w) | fdisk $install_device 
+
+(echo g; echo w) | fdisk $install_device
+
+# Create partition 1
+(echo n; echo 1; echo ""; echo "+500M"; echo w) | fdisk $install_device 
+(echo t; echo $EFI_SYSTEM_NUMBER; echo w) | fdisk $install_device 
+
+# Create partition 2
+(echo n; echo 2; echo ""; echo "+500M"; echo w) | fdisk $install_device 
+(echo t; echo 2; echo $LINUX_FILESYSTEM_NUMBER; echo w) | fdisk $install_device 
+
+# Create partition 3
+(echo n; echo 3; echo ""; echo ""; echo w) | fdisk $install_device 
+(echo t; echo 3; echo $LINUX_LVM_NUMBER; echo w) | fdisk $install_device 
+
+
 
 mkfs.ext2 $install_device"1" -L boot
 
