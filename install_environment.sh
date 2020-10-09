438d4c47ab954bfc43c3b643b3d3cc8bd8355071
Jan Erichsen <jan@erichsen-web.de>
2020-10-09 22:44:06 +0200

    fixed argument

--- install_environment.sh
+++ install_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 pacman -S --noconfirm ansible
 
-mkdir -R /tmp/arch-install-script
+mkdir -p /tmp/arch-install-script
 
 curl https://github.com/NotoriousBIT/arch-install-script/blob/master/playbook.yml -L -o /tmp/arch-install-script/playbook.yml
 
