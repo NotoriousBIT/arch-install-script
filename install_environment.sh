2ca7f3891b2db6b736274f807a00f82289ee142a
Jan Erichsen <jan@erichsen-web.de>
2020-10-09 22:37:38 +0200

    test ansible playbook

--- install_environment.sh
+++ install_environment.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 pacman -S --noconfirm ansible
+
+curl https://github.com/NotoriousBIT/arch-install-script/blob/master/playbook.yml -L -o /tmp/arch-install/script/playbook.yml
+
+ansible-playbook --ask-become-pass /tmp/arch-install/script/playbook.yml
