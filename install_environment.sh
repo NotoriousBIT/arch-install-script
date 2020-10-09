80071fe3b44aa4153cc8627be4ed75bfb90709d8
Jan Erichsen <jan@erichsen-web.de>
2020-10-09 22:39:20 +0200

    fixed not existing path

--- install_environment.sh
+++ install_environment.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 pacman -S --noconfirm ansible
 
+mkdir -R /tmp/arch-install/
+
 curl https://github.com/NotoriousBIT/arch-install-script/blob/master/playbook.yml -L -o /tmp/arch-install/script/playbook.yml
 
 ansible-playbook --ask-become-pass /tmp/arch-install/script/playbook.yml
