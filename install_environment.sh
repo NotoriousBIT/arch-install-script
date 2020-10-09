27d3d87e875fc805386f7c4e9fef0e4b4c3f15a7
Jan Erichsen <jan@erichsen-web.de>
2020-10-09 22:40:39 +0200

    fixed path again

--- install_environment.sh
+++ install_environment.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 pacman -S --noconfirm ansible
 
-mkdir -R /tmp/arch-install/
+mkdir -R /tmp/arch-install-script
 
-curl https://github.com/NotoriousBIT/arch-install-script/blob/master/playbook.yml -L -o /tmp/arch-install/script/playbook.yml
+curl https://github.com/NotoriousBIT/arch-install-script/blob/master/playbook.yml -L -o /tmp/arch-install-script/playbook.yml
 
-ansible-playbook --ask-become-pass /tmp/arch-install/script/playbook.yml
+ansible-playbook --ask-become-pass /tmp/arch-install-script/playbook.yml
