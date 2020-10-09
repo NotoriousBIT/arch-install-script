00c900135e471dcca050050d6f8314fe104dd030
Jan Erichsen <jan@erichsen-web.de>
2020-10-09 22:46:54 +0200

    Fixed playbook URL

--- install_environment.sh
+++ install_environment.sh
@@ -3,6 +3,6 @@ pacman -S --noconfirm ansible
 
 mkdir -p /tmp/arch-install-script
 
-curl https://github.com/NotoriousBIT/arch-install-script/blob/master/playbook.yml -L -o /tmp/arch-install-script/playbook.yml
+curl https://raw.githubusercontent.com/NotoriousBIT/arch-install-script/master/playbook.yml -L -o /tmp/arch-install-script/playbook.yml
 
 ansible-playbook --ask-become-pass /tmp/arch-install-script/playbook.yml
