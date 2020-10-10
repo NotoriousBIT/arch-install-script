78a377472cd4d8b9d493d3e59bd3a645b8278a18
Jan Erichsen <jan@erichsen-web.de>
2020-10-10 20:54:01 +0200

    prevent curl from using cache

--- install_environment.sh
+++ install_environment.sh
@@ -3,6 +3,6 @@ sudo pacman -S --noconfirm ansible
 
 mkdir -p /tmp/arch-install-script
 
-curl https://raw.githubusercontent.com/NotoriousBIT/arch-install-script/master/playbook.yml -L -o /tmp/arch-install-script/playbook.yml
+curl -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/NotoriousBIT/arch-install-script/master/playbook.yml -L -o /tmp/arch-install-script/playbook.yml
 
 ansible-playbook --ask-become-pass /tmp/arch-install-script/playbook.yml
